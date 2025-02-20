38088b434ecf6883a0088e0045d112fd71a408bd
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-02-20 18:01:42 +0100

    feat(IA-1): Refactor finWinner, initializeGameBoard and remove unncessary code

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
+// ? constantes para el tablero de juego
 const table = document.getElementById("game-table");
 
+// ? constantes para las imágenes del tablero
 const xImage =
   "http://cdn.iahorro.com/internal-resources/it-technical-test/x.png";
 const oImage =
@@ -9,12 +11,20 @@ const alternativeXImage =
 const alternativeOImage =
   "http://cdn.iahorro.com/internal-resources/it-technical-test/o2.png";
 
+// ? constantes para los subtulos del tablero
 const startSubtitle = document.getElementById("start-game-subtitle");
 const nextPlayerSubtitle = document.getElementById("next-player-subtitle");
-const nextPlayerSpan = document.getElementById("next-player");
+
+const xWinsElement = document.getElementById("x-wins");
+const oWinsElement = document.getElementById("o-wins");
+
+// ? constante para el botón de nuevo juego
+const newGameButton = document.getElementById("new-game-button");
+
+// ? constante para el span de siguiente jugador
+const nextPlayerElement = document.getElementById("next-player");
 
 let isAlternativeImages = false;
-const newGameButton = document.getElementById("new-game-button");
 
 // ? Crea un nuevo tablero de juego
 let stateTable = createNewStateTable();
@@ -24,28 +34,28 @@ let winner = null;
 let xWins = 0;
 let oWins = 0;
 
-// ? Función para inicializar los subtitulos y mostrar !empezamos!
+// * Función para inicializar los subtitulos y mostrar !empezamos!
 function initializeSubtitles() {
   startSubtitle.style.display = "block";
   nextPlayerSubtitle.style.display = "none";
 }
 
-// ? Función para actualizar el subtitulo de siguiente jugador y mostrar la imagen del jugador
+// * Función para actualizar el subtitulo de siguiente jugador y mostrar la imagen del jugador
 function updateNextPlayerDisplay() {
   startSubtitle.style.display = "none";
   nextPlayerSubtitle.style.display = "block";
   const currentImages = getCurrentImages();
-  nextPlayerSpan.innerHTML = `<img src="${currentImages[currentPlayer]}" class="player-icon">`;
+  nextPlayerElement.innerHTML = `<img src="${currentImages[currentPlayer]}" class="player-icon">`;
 }
 
-// ? Función para obtener las imágenes actuales
+// * Función para obtener las imágenes actuales
 function getCurrentImages() {
   return isAlternativeImages
     ? { x: alternativeXImage, o: alternativeOImage }
     : { x: xImage, o: oImage };
 }
 
-// ? Función para cambiar las imágenes
+// * Función para cambiar las imágenes
 function toggleImages() {
   isAlternativeImages = !isAlternativeImages;
   const images = getCurrentImages();
@@ -65,12 +75,12 @@ function toggleImages() {
   document.querySelector(".info-score-item:last-child img").src = images.o;
 }
 
-// ? Función para crear un nuevo tablero de juego
+// * Función para crear un nuevo tablero de juego
 function createNewStateTable() {
   return Array.from({ length: 3 }, () => Array(3).fill(null));
 }
 
-// TODO Función para rellenar el tablero según el estado del juego
+// * Función para rellenar el tablero según el estado del juego
 function populate(state) {
   if (!table) {
     console.log("No se ha encontrado game-table");
@@ -86,7 +96,7 @@ function populate(state) {
   });
 }
 
-// TODO Función para determinar qué jugador debe ser el siguiente
+// * Función para determinar qué jugador debe ser el siguiente
 function nextPlayer(state) {
   let xCount = 0;
   let oCount = 0;
@@ -99,35 +109,63 @@ function nextPlayer(state) {
   return xCount === oCount ? "x" : "o";
 }
 
-// TODO Función para determinar si hay un ganador en el juego actual
+// * Función para determinar si hay un ganador en el juego actual
 function findWinner(state) {
-  let winner = null;
-  let rowData = null;
+  // ? Chequear filas y columnas juntas
+  // ? Hago el return cada if para que no se siga ejecutando el bucle
 
-  // TODO Chequear filas
-  state.forEach((row) => {
-    console.log("row", row);
-    rowData = row;
-    if (row[0] === row[1] && row[1] === row[2]) {
-      winner = row[0];
+  // ? Para las filas introduzco el index en el primer parametro de state[i]
+  for (let i = 0; i < 3; i++) {
+    // ? Devuelve el ganador en una fila
+    if (
+      state[i][0] &&
+      state[i][0] === state[i][1] &&
+      state[i][1] === state[i][2]
+    ) {
+      return state[i][0];
     }
-  });
 
-  // TODO Chequear columnas
-  for (let i = 0; i < state.length; i++) {
-    if (state[0][i] === state[1][i] && state[1][i] === state[2][i]) {
-      winner = state[0][i];
+    // ? Para las columnas introduzco el index en el segundo parametro de state[0][i]
+    // ? Devuelve el ganador en una columna
+    if (
+      state[0][i] &&
+      state[0][i] === state[1][i] &&
+      state[1][i] === state[2][i]
+    ) {
+      return state[0][i];
     }
   }
 
-  // TODO Chequear diagonales
-  if (state[0][0] === state[1][1] && state[1][1] === state[2][2]) {
-    winner = state[0][0];
+  // ? Chequear diagonales
+  if (
+    (state[0][0] === state[1][1] && state[1][1] === state[2][2]) ||
+    (state[0][2] === state[1][1] && state[1][1] === state[2][0])
+  ) {
+    // ? Devuelve el ganador en una diagonal
+    return state[1][1];
   }
-  if (state[0][2] === state[1][1] && state[1][1] === state[2][0]) {
-    winner = state[0][2];
+
+  return null;
+}
+
+// * Función para actualizar el marcador
+function updateScore() {
+  xWinsElement.innerHTML = xWins;
+  oWinsElement.innerHTML = oWins;
+}
+
+// ? Creao esta función porque es buena práctica no tener el código de inicialización en el global
+// ? Además es más legible y fácil de mantener
+
+// * Asociar eventos a cada casilla del tablero
+function initializeGameBoard() {
+  for (let i = 0; i < table.rows.length; i++) {
+    for (let j = 0; j < table.rows[i].cells.length; j++) {
+      // ? Asocio el evento onClick a la función handleClick por que he adaptado a la nueva funcionalidad y hace lo mismo
+      // ? así no duplico código y es más escalable y reutilizable
+      table.rows[i].cells[j].onclick = handleClick;    
+    }
   }
-  return winner;
 }
 
 // ? Rellenar el tablero según el estado del juego
@@ -139,64 +177,41 @@ newGameButton.disabled = true;
 // ? Inicializar los subtitulos
 initializeSubtitles();
 
-// TODOsociar eventos a cada casilla del tablero
-for (let i = 0; i < table.rows.length; i++) {
-  for (let j = 0; j < table.rows[i].cells.length; j++) {
-    table.rows[i].cells[j].onclick = function () {
-      //  TODO Si la casilla está vacía y no hay un ganador
-      if (stateTable[i][j] === null && winner === null) {
-        stateTable[i][j] = currentPlayer;
-        this.innerHTML = currentPlayer;
-        winner = findWinner(stateTable);
-        if (winner === "x") {
-          xWins++;
-          alert("X wins!");
-        } else if (winner === "o") {
-          oWins++;
-          alert("O wins!");
-        } else {
-          currentPlayer = nextPlayer(stateTable);
-          document.getElementById("next-player").innerHTML = currentPlayer;
-        }
-        document.getElementById("x-wins").innerHTML = xWins;
-        document.getElementById("o-wins").innerHTML = oWins;
-      }
-    };
-  }
-}
+initializeGameBoard();
 
 // ? He borrado la creación del botón por que creo que es mejor hacer en html por que siempre va a ser visible
 // ? También he borrado la funcionalidad por que esta duplicada enla función reset
 
-// TODO Función para determinar si hay un ganador o un empate
+// * Función para determinar si hay un ganador o un empate
 function play() {
   winner = findWinner(stateTable);
   if (winner) {
-    alert(winner + " wins!");
-    if (winner === "x") {
-      xWins++;
-    } else {
-      oWins++;
-    }
-    document.getElementById("x-wins").innerHTML = xWins;
-    document.getElementById("o-wins").innerHTML = oWins;
+    alert(winner + " wins!");    
+    winner === "x" ? xWins++ : oWins++;
+    updateScore();
   } else if (checkTie()) {
     alert("Tie!");
   } else {
     currentPlayer = nextPlayer(stateTable);
-    document.getElementById("next-player").innerHTML = currentPlayer;
+    nextPlayerElement.innerHTML = currentPlayer;
     updateNextPlayerDisplay();
   }
 }
 
-// ? Función para actualizar el color de la celda
-function updateCellColor(cell, player) {
+// * Función para borrar las clases de las celdas
+function clearCellClasses(cell) {
   cell.classList.remove(
+    "winner",
     "x-cell",
     "o-cell",
     "x-cell-alternative",
     "o-cell-alternative"
   );
+}
+
+// * Función para actualizar el color de la celda
+function updateCellColor(cell, player) {
+  clearCellClasses(cell);
   const colorClass = isAlternativeImages
     ? `${player}-cell-alternative`
     : `${player}-cell`;
@@ -204,7 +219,7 @@ function updateCellColor(cell, player) {
 }
 
 // TODOFunción para manejar el evento onClick
-function onClick() {
+function handleClick() {
   newGameButton.disabled = false;
   if (this.innerHTML === "" && winner === null) {
     // ? oculta el subtitulo de inicio y muestra el de siguiente jugador
@@ -224,59 +239,57 @@ function onClick() {
     if (winner) {
       this.classList.add("winner");
       play();
+      updateScore();
     } else if (checkTie()) {
       play();
+      updateScore();
     } else {
       currentPlayer = nextPlayer(stateTable);
-      document.getElementById("next-player").innerHTML = currentPlayer;
+      nextPlayerElement.innerHTML = currentPlayer;
       updateNextPlayerDisplay();
     }
   }
 }
 
-// TODO Función para reiniciar el juego
+// * Función para reiniciar el juego
 function reset() {
   //? resetea el tablero de juego
   stateTable = createNewStateTable();
   currentPlayer = "x";
   winner = null;
+  newGameButton.disabled = true;
 
   // ? Convierto los elementos td en un array y recorro cada uno para limpiarlos
+  // ! Array from convierte la colleccion de HTML en un array paara poder reocrrerlo
+  // ! y resetaer los valores de cada celda
   Array.from(table.getElementsByTagName("td")).forEach((cell) => {
     cell.innerHTML = "";
-    cell.classList.remove(
-      "winner",
-      "x-cell",
-      "o-cell",
-      "x-cell-alternative",
-      "o-cell-alternative"
-    );
+    clearCellClasses(cell);
   });
 
   // ? Inicializa los subtitulos
   initializeSubtitles();
 
-  document.getElementById("next-player").innerHTML = currentPlayer;
+  nextPlayerElement.innerHTML = currentPlayer;
 }
 
-// TODO Función para determinar si hay un empate
+// * Función para determinar si hay un empate
+// ! Con el metodo flat() convierto el tablero en un array plano
+// ! El método .every() verifica si todos los elementos de un array cumplen una condición.
+// ! Devuelve true si todos cumplen la condición, y false si al menos uno no la cumple.
+
 function checkTie() {
-  for (let i = 0; i < stateTable.length; i++) {
-    for (let j = 0; j < stateTable[i].length; j++) {
-      if (stateTable[i][j] === null) {
-        return false;
-      }
-    }
-  }
-  return true;
+  // ? Con every comprueba que todas las celdas sean distintas de null y devuelve true si es así
+  return stateTable.flat().every((cell) => cell !== null);
 }
 
+// ? He borrado el código por que ya no es necesario
 // TODO Añadir eventos onClick a cada casilla del tablero
-for (let i = 0; i < table.rows.length; i++) {
-  for (let j = 0; j < table.rows[i].cells.length; j++) {
-    table.rows[i].cells[j].onclick = onClick;
-  }
-}
+// for (let i = 0; i < table.rows.length; i++) {
+//   for (let j = 0; j < table.rows[i].cells.length; j++) {
+//     table.rows[i].cells[j].onclick = onClick;
+//   }
+// }
 
 // ? Ya no es necesario añadir evento onClick al botón "Nuevo juego"
 // newGameButton.onclick = reset;
